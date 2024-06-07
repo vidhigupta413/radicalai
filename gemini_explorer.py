5721ef8c0de07277fc11974a59caceacdc63d491
vidhigupta413 <67328330+vidhigupta413@users.noreply.github.com>
2024-06-07 12:33:22 -0400

    Update gemini_explorer.py

--- gemini_explorer.py
+++ gemini_explorer.py
@@ -1,1 +1,18 @@
+import vertexai
+import streamlit as st
+from vertexai.preview import generative_models
+from vertexai.preview.generative_models import GenerativeModel, Part, Content, ChatSession
 
+project = "radical-ai-task-1"
+vertexai.init(project * project)
+
+config = generative_models.GenerationConfig(
+    temperature = 0.4
+)
+#Load model with config
+model = GenerativeModel(
+    "gemini-pro",
+    generation_config = config
+
+)
+chat = model.start_chat()
